8d8b17d5415648a1cd76dabfdf755f6e4aab5847
Priscilo <psiphonblue@gmail.com>
2024-08-22 18:55:30 -0500

    mensaje

---
+++ metod.py
@@ -0,0 +1,9 @@
+def saludar(nombre):
+    """Esta función imprime un saludo perzsonalizado."""
+    print("Hola,", nombre, ("!")
+
+
+
+          # Llamada a la función
+          saludar("Juan")
+          
---
+++ metodo2.py
@@ -0,0 +1,26 @@
+1 #Función sin parametro
+2  def saludoBienvenida():
+3   print("|BIENVENIDO")
+4 #Función con parametro
+5 def saludoNombre(name):
+6    print("Hola!", name)
+8
+9  #Usando las funciones con los argomentos
+10 #SaludoBienvenida()
+11 #saludoNombre("Priscilo")
+12
+13   print("================")
+14 def saludoPersonalizado(nombre,sexo):
+15   if(sexo == "Masculino"):
+16       print(f"Hola {nombre} Bienvenida señorita")
+17 saludoPersonalizado("Maria","Femenino"):
+18  saludoPersonalizado("Juan","Masculino"):
+19
+20       print("================")
+21       def suma(a,b):
+22         resultado = a+b
+23           return resultado
+24
+25        print(suma(2,2))
+        
+        
